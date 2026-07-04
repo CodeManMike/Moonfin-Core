c25c4dc19e6a99695ff6ff622e7645777719d2b4
Michael Brian Du Preez <125465986+CodeManMike@users.noreply.github.com>
2026-07-04 15:44:59 +0200

    Add GenreNameListTile focusable text row widget

--- lib/ui/widgets/genre_name_list_tile.dart
+++ lib/ui/widgets/genre_name_list_tile.dart
@@ -1,6 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:moonfin_design/moonfin_design.dart';
+
+import '../../util/focus/dpad_keys.dart';
+import '../mixins/focus_state_mixin.dart';
+
 class GenreListItem {
   final String id;
   final String name;
 
   GenreListItem({required this.id, required this.name});
+}
+
+class GenreNameListTile extends StatefulWidget {
+  final GenreListItem genre;
+  final bool selected;
+  final VoidCallback onTap;
+  final ValueChanged<bool>? onFocusChange;
+
+  const GenreNameListTile({
+    super.key,
+    required this.genre,
+    required this.selected,
+    required this.onTap,
+    this.onFocusChange,
+  });
+
+  @override
+  State<GenreNameListTile> createState() => _GenreNameListTileState();
+}
+
+class _GenreNameListTileState extends State<GenreNameListTile>
+    with FocusStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    final isHighlighted = widget.selected || showFocusBorder;
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      onEnter: (_) => setHovered(true),
+      onExit: (_) => setHovered(false),
+      child: Focus(
+        onFocusChange: (focused) {
+          setFocused(focused);
+          widget.onFocusChange?.call(focused);
+        },
+        onKeyEvent: (_, event) {
+          if (isActivateKey(event)) {
+            widget.onTap();
+            return KeyEventResult.handled;
+          }
+          return KeyEventResult.ignored;
+        },
+        child: GestureDetector(
+          onTap: widget.onTap,
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            color: isHighlighted
+                ? AppColorScheme.accent.withAlpha(40)
+                : Colors.transparent,
+            child: Text(
+              widget.genre.name,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: isHighlighted ? FontWeight.w600 : FontWeight.w400,
+                color: AppColorScheme.onSurface,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
--- test/ui/widgets/genre_name_list_tile_test.dart
+++ test/ui/widgets/genre_name_list_tile_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:moonfin/ui/widgets/genre_name_list_tile.dart';
 
@@ -7,5 +8,33 @@ void main() {
 
     expect(genre.id, 'g1');
     expect(genre.name, 'Comedy');
+  });
+
+  group('GenreNameListTile', () {
+    testWidgets('renders genre name and calls onTap when activated', (
+      tester,
+    ) async {
+      var tapped = false;
+      final genre = GenreListItem(id: 'g1', name: 'Comedy');
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: GenreNameListTile(
+              genre: genre,
+              selected: false,
+              onTap: () => tapped = true,
+            ),
+          ),
+        ),
+      );
+
+      expect(find.text('Comedy'), findsOneWidget);
+
+      await tester.tap(find.byType(GenreNameListTile));
+      await tester.pumpAndSettle();
+
+      expect(tapped, true);
+    });
   });
 }
